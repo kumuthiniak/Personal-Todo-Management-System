69e187fa4e76036338c491b91550dc07fe91416d
kumuthiniak <kumuthini.program@gmail.com>
2025-09-05 17:32:37 +0530

    Initial commit - Spring Boot Todo App

---
+++ src/main/java/com/example/todo/PersonalTodoManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalTodoManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalTodoManagementSystemApplication.class, args);
+	}
+
+}
